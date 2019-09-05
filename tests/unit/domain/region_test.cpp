ac38911ff181808f5d2372f9865c5062446e880a
genshen <genshenchu@gmail.com>
2019-09-06 00:10:16 +0800

    test(region): add more tests to test region assignment operator.
    Unfortunately, assignment from function return value will cause a wrong reference (in test
    region_assign_scope2_test.region_test), test is not passed.

--- tests/unit/domain/region_test.cpp
+++ tests/unit/domain/region_test.cpp
@@ -27,6 +27,10 @@ TEST(region_assign_test, region_test) {
     EXPECT_EQ(region_ass.y_high, 2);
     EXPECT_EQ(region_ass.z_high, 3);
 
+    EXPECT_EQ(&(region.x_high), &(region.high[0]));
+    EXPECT_EQ(&(region.y_high), &(region.high[1]));
+    EXPECT_EQ(&(region.z_high), &(region.high[2]));
+
     region_ass = comm::Region<int>(1, 2, 3, -1, -2, -3);
     EXPECT_EQ(region_ass.x_low, 1);
     EXPECT_EQ(region_ass.y_low, 2);
@@ -34,4 +38,39 @@ TEST(region_assign_test, region_test) {
     EXPECT_EQ(region_ass.x_high, -1);
     EXPECT_EQ(region_ass.y_high, -2);
     EXPECT_EQ(region_ass.z_high, -3);
+
+    EXPECT_EQ(&(region_ass.x_high), &(region_ass.high[0]));
+    EXPECT_EQ(&(region_ass.y_high), &(region_ass.high[1]));
+    EXPECT_EQ(&(region_ass.z_high), &(region_ass.high[2]));
+}
+
+TEST(region_assign_scope_test, region_test) {
+    comm::Region<int> region;
+    {
+        region = comm::Region<int>{-1, -2, -3, 1, 2, 3};
+    }
+    printf("%p %d %p %d\n", &(region.z_high), region.z_high, &(region.high[2]), region.high[2]);
+    EXPECT_EQ(region.x_low, -1);
+    EXPECT_EQ(region.y_low, -2);
+    EXPECT_EQ(region.z_low, -3);
+    EXPECT_EQ(region.x_high, 1);
+    EXPECT_EQ(region.y_high, 2);
+    EXPECT_EQ(region.z_high, 3);
+}
+
+std::vector<comm::Region<int> > returnRegionTest() {
+    return {
+            {-1, -2, -3, 1, 2, 3}
+    };
+}
+
+TEST(region_assign_scope2_test, region_test) {
+    std::vector<comm::Region<int> > regions = returnRegionTest();
+    printf("%p  %p\n", &(regions[0].z_high), &(regions[0].high[2]));
+    EXPECT_EQ(&(regions[0].x_low), &(regions[0].low[0]));
+    EXPECT_EQ(&(regions[0].y_low), &(regions[0].low[1]));
+    EXPECT_EQ(&(regions[0].z_low), &(regions[0].low[2]));
+    EXPECT_EQ(&(regions[0].x_high), &(regions[0].high[0]));
+    EXPECT_EQ(&(regions[0].y_high), &(regions[0].high[1]));
+    EXPECT_EQ(&(regions[0].z_high), &(regions[0].high[2]));
 }
